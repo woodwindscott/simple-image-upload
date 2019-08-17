03a4c89a091924d1880ddfd03eb804b5404222bb
Scott Moore <woodwindscott@yahoo.com>
2019-08-17 11:59:35 -0400

    Added display photo button and functionality

--- logic.js
+++ logic.js
@@ -15,7 +15,7 @@ var firebaseConfig = {
 
   var database = firebase.database;
 
-
+  var temp = "";
 
   document.querySelector('.file-select').addEventListener('change', handleFileUploadChange);
   document.querySelector('.file-submit').addEventListener('click', handleFileUploadSubmit);
@@ -34,32 +34,40 @@ var firebaseConfig = {
       console.log(error);
     }, () => {
        // Do something once upload is complete
-       var temp = selectedFile.name;
+       temp = selectedFile.name;
        console.log(temp);
        console.log('success');
     });
   }
 
-  storageRef.child('images/IMG_0100.JPG').getDownloadURL().then(function(url) {
-    // `url` is the download URL for 'images/stars.jpg'
 
-    // This can be downloaded directly:
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'blob';
-    xhr.onload = function(event) {
-      var blob = xhr.response;
-    };
-    xhr.open('GET', url);
-    xhr.send();
-    console.log(url);
 
-    $("#picture-display").html("<img src='" + url + "' width='200'>");
-  
-    // Or inserted into an <img> element:
-    var img = document.getElementById('myimg');
-    img.src = url;
-  }).catch(function(error) {
-    // Handle any errors
+  $(".display-photo").on("click", function() {
+
+
+    storageRef.child('images/' + temp).getDownloadURL().then(function(url) {
+        // `url` is the download URL for 'images/stars.jpg'
+
+        // This can be downloaded directly:
+        var xhr = new XMLHttpRequest();
+        xhr.responseType = 'blob';
+        xhr.onload = function(event) {
+        var blob = xhr.response;
+        };
+        xhr.open('GET', url);
+        xhr.send();
+        console.log(url);
+
+        $("#picture-display").html("<img src='" + url + "' width='200'>");
+    
+        // Or inserted into an <img> element:
+        var img = document.getElementById('myimg');
+        img.src = url;
+    }).catch(function(error) {
+        // Handle any errors
+    });
+
   });
 
-//   $("#picture-display").html('img src="' + url + "'");
+
+
